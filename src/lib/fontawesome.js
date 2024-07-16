fc7e350783e1e1b5c56e42358518e59bf7480947
Takkino31 <57909904+Takkino31@users.noreply.github.com>
2024-07-16 21:56:44 +0000

    Font awesome update

--- src/lib/fontawesome.js
+++ src/lib/fontawesome.js
@@ -1,5 +1,6 @@
-// lib/fontawesome.js
+// fontawesome.js
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faCog, faQuestionCircle, faBell, faUserCircle, faThumbsUp, faThumbsDown, faHome, faTags, faBookmark, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
-library.add(fas);
+library.add(faSearch, faCog, faQuestionCircle, faBell, faUserCircle, faThumbsUp, faThumbsDown, faHome, faTags, faBookmark, faUsers, faFacebook, faTwitter, faInstagram);
